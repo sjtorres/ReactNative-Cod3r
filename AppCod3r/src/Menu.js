38ecd7b45bc1ec00c0723a2009a1150adb34bbd4
Sandro Torres <sandrotorres.ti@gmail.com>
2020-03-20 07:34:26 -0300

    Eventos e Componentes Controlados

--- AppCod3r/src/Menu.js
+++ AppCod3r/src/Menu.js
@@ -6,9 +6,18 @@ import ParImpar from './componentes/ParImpar';
 import {Inverter, MegaSena} from './componentes/Multi';
 import Contador from './componentes/Contador';
 import Plataformas from './componentes/Plataformas';
+import ValidarProps from './componentes/ValidarProps';
+import Evento from './componentes/Evento';
 
 export default createDrawerNavigator(
   {
+    Evento: {
+      screen: Evento,
+    },
+    ValidarProps: {
+      screen: () => <ValidarProps ano={20} />,
+      navigationOptions: {title: 'Validar Props'},
+    },
     Platafomas: {
       screen: Plataformas,
     },
